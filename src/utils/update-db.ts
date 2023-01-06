2434bf19d2210361e4c04d863738ab718d35da71
ilonaK <ikosko@alfabank.ru>
2023-01-06 22:49:43 +0300

    refactor: change udpadeDB function

--- src/utils/update-db.ts
+++ src/utils/update-db.ts
@@ -1,24 +1,13 @@
 import path from 'path';
-import { writeFile, readFile } from 'fs/promises';
+import { writeFile } from 'fs/promises';
 
 import { User } from '../types';
 
-export const updateDB = async (parsedData: User) => {
+export const updateDB = async (users: User[]) => {
   const pathToTheFile = path.resolve(process.cwd(), './db.json');
 
   try {
-    const content = await readFile(pathToTheFile, {
-      encoding: 'utf8',
-      flag: 'a+',
-    });
-
-    const users: User[] = (
-      content ? JSON.parse(content).concat(parsedData) : [parsedData]
-    );
-      
-    const promise = writeFile(pathToTheFile, JSON.stringify(users));
-
-    await promise;
+    await writeFile(pathToTheFile, JSON.stringify(users));
   } catch (err) {
     console.error(err);
   }
